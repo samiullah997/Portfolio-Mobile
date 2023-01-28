88e79dbecfed0d81a61d5bd799814e31e4e83c58
samiullah997 <samiullahk997@gmail.com>
2023-01-29 02:53:40 +0500

    update script.js file

--- script.js
+++ script.js
@@ -94,42 +94,42 @@ const showItemInfo = [
     liveSource: 'https://github.com/samiullah997/To-Do-List.git/',
   },
   {
-    title: 'Multi-Post Stories 2',
-    image: './images/img-placeholder-1.png',
-    desc: "A sami daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
+    title: 'Book an Appointment',
+    image: './images/book_an_appointment.png',
+    desc: "Bike reservation is a web application that allows users to view and reserve a bike, i.e. Yamaha, Suzuki Honda e.t.c. A user can reserve one bike per session on separate dates.",
+    technologies: ['tailwind', 'html', 'react', 'redux', 'JavaScript', 'api'],
+    liveVersion: 'https://deploy-preview-13--gilded-begonia-47c85d.netlify.app',
+    liveSource: 'https://github.com/samiullah997/book_an_appointment_front_end',
+  },
+  {
+    title: 'Budget App',
+    image: './images/bugdet_app.png',
+    desc: "Budget App is a web application that allows users to track their expenses and income. A user can add their income and expenses and the app will calculate the balance.",
+    technologies: ['tailwind', 'html', 'ruby on rails'],
     liveVersion: 'https://samiullah997.github.io/Portfolio-Mobile/',
     liveSource: 'https://github.com/samiullah997/Portfolio-Mobile',
   },
   {
-    title: 'Multi-Post Stories',
-    image: './images/img-placeholder-1.png',
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    liveVersion: 'https://samiullah997.github.io/Portfolio-Mobile/',
-    liveSource: 'https://github.com/samiullah997/Portfolio-Mobile',
+    title: 'Game of Life',
+    image: './images/game_of_life.png',
+    desc: "In this project, I am building a simple HTML list of To Do tasks. The list will be styled according to the specifications. This simple web page will be built using webpack and served by a webpack dev server.",
+    technologies: ['css', 'html', 'scss', 'webpack', 'JavaScript'],
+    liveVersion: 'https://duanedave.github.io/Capstone-Kanban',
+    liveSource: 'https://github.com/samiullah997/Capstone-Kanban',
   },
   {
-    title: 'Multi-Post Stories',
-    image: './images/img-placeholder-1.png',
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    liveVersion: 'https://samiullah997.github.io/Portfolio-Mobile/',
-    liveSource: 'https://github.com/samiullah997/Portfolio-Mobile',
-  },
-  {
-    title: 'Multi-Post Stories',
-    image: './images/img-placeholder-1.png',
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    liveVersion: 'https://samiullah997.github.io/Portfolio-Mobile/',
-    liveSource: 'https://github.com/samiullah997/Portfolio-Mobile',
+    title: 'Book Store',
+    image: './images/book_store.png',
+    desc: "In this project I am building a Bookstore CMS. The app will allow users to add books, remove books, and filter books by category. This simple web page will be built using React and Redux.",
+    technologies: ['css', 'html', 'react', 'redux', 'JavaScript'],
+    liveVersion: 'https://subtle-brigadeiros-d2870f.netlify.app',
+    liveSource: 'https://github.com/samiullah997/bookstore',
   },
 ];
 
 const setCardData = document.getElementById('recent-frame-2');
 showItemInfo.forEach((itemsData, position) => {
-  let content = `<div class="card-work media-card-${position + 1}" id="card-work">`;
+  let content = `<div class="card-work media-card card-${position}" style="background-image: url(${itemsData.image});" id="card-work">`;
   content += '<div class="post-right-block">';
   content += `<h1 class="card-post-title" id="post-title">${itemsData.title}</h1>`;
   content += '<p class="supporting-text" id="post-desc">';
@@ -141,7 +141,7 @@ showItemInfo.forEach((itemsData, position) => {
   });
   content += '</ul>';
   content += '</div>';
-  content += `<div class="btn-action" data-modal-target="#modal" id="show-modal-${position + 1}">`;
+  content += `<div class="btn-action" data-modal-target="#modal" id="show-modal-${position}">`;
   content += '<div class="post-btn-enable-2">';
   content += '<a class="btn-text">See Project</a>';
   content += '</div>';
@@ -167,7 +167,7 @@ function setData(Data) {
 }
 
 showItemInfo.forEach((itemsData2, ind) => {
-  document.getElementById('show-modal-'.concat(ind)).addEventListener('click', () => {
+  document.getElementById(`show-modal-${ind}`).addEventListener('click', () => {
     setData(itemsData2);
   });
 });
